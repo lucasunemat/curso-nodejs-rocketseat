eb0b5991bf957645bf2700fbbbedb3291153a87b
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-01-12 13:48:07 -0400

    termino explicação

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -58,7 +58,7 @@ class OneToHundredStrem extends Readable {
                 //push serve readable streamfornecer informações para quem estiver consumindo
                 //ao enviar nulo digo que nao tenho mais info para enviar
                 this.push(null);
-            } else {
+            } else { //aqui é tipo o que eu quero mostrar no stdout, o que ta sendo processado por ele
                 const buf = Buffer.from(String(i)) //envio a informação que quero mostrar no stdout convertida em string (buffer só aceita string)
                 this.push(buf)
             }
