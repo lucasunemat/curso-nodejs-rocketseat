409eb47e5007ff229e94917f5ebee8e7a194af9d
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-01-13 19:17:42 -0400

    treinando pegar dados do body e criar user

--- src/server.js
+++ src/server.js
@@ -24,22 +24,42 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url } = req; //desestruturando req e obtendo method e url dele
+
+    //crio um array de buffers (to usando isso porque quero dar um console log no body da requisição no terminal)
+    const buffers = [];
+
+    //juntando todos os pedaços que vieram da req (chunks)
+    for await (const chunk of req) {
+        buffers.push(chunk);
+    }
+
+    try {
+        //se tem body, converte para string e depois para json
+        req.body = JSON(Buffer.concat(buffers).toString());
+    } catch {
+        //se não tem body, seta como null
+        req.body = null;
+    }
+
+    //isso aqui faz aparecer o "POST /users" no terminal 
     console.log(method, url);
 
     //JSON é variavel global do node
     if (method === 'GET' && url === '/users') {
         return res
-        .setHeader('Content-Type', 'application/json') //setando header da resposta indicando para o front que é um json | facilita o navegador a entender o que está sendo retornado e formatar
-        .end(JSON.stringify(users));
+            .setHeader('Content-Type', 'application/json') //setando header da resposta indicando para o front que é um json | facilita o navegador a entender o que está sendo retornado e formatar
+            .end(JSON.stringify(users));
     }
 
     if (method === 'POST' && url === '/users') {
+        const { name, email } = body;
+
         users.push({
             id: 1,
-            name: 'John Doe',
-            email: 'johndoe@gmail.com!'
+            name, //to adicionando o nome que veio da requisição. é o mesmo que colocar name: name
+            email,
         })
         return res.writeHead(201).end('Criando usuários!');
         //CRIOU algo com sucessso = 201. Usamos writeHead para escrever o cabeçalho da resposta
