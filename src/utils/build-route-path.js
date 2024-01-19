bd6b58d0ee7b70f59956ce6282e26e4b1022e21d
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-01-19 18:14:04 -0400

    obs

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -15,6 +15,7 @@ export function buildRoutePath(path) {
     // substitui O QUE DER MATCH (ou seja, o que tiver :sjhfsjfsdkf) pelo que está dentro do parênteses
     // para nomear grupos, usa ?<nomeDoGrupo>
     // ?<$1> indica que quero pegar o primeiro retorno (linha 12, após os dois pontos) e usar como nome do grupo
+    // aqui vai os parâmetros regex obrigatorios, pelo jeito
     const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
      //const teste2 = /\/users\/([a-z0-9\-_]+)/
@@ -25,6 +26,8 @@ export function buildRoutePath(path) {
     // o ? depois do () indica que o grupo é opcional
     // o $ no final indica que depois da query é obrigatorio ser o final da string
     // .* indica que pode ter qualquer caracter infinitas vezes após o ?
+    // Abaixo eu faço esse grupo pathWithParams ser o que vai vir depois do /users/, e adiciono também os 
+    // grupos opcionais que podem vir depois da query
     const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
 
     // /^(?<query>\\?(.*))?$/
